4ee69b2543f5c17f38ee9570f4b1418161aea64c
A-N-O-D-E-R <weber.anoder@proton.me>
2025-11-16 00:16:29 +0100

    :sparkles: add support for all steps

--- src/test/java/com/anode/workflow/spring/autoconfigure/impl/DefaultWorkflowComponentFactoryTest.java
+++ src/test/java/com/anode/workflow/spring/autoconfigure/impl/DefaultWorkflowComponentFactoryTest.java
@@ -1,5 +1,6 @@
 package com.anode.workflow.spring.autoconfigure.impl;
 
+import com.anode.workflow.entities.steps.InvokableRoute;
 import com.anode.workflow.entities.steps.InvokableTask;
 import com.anode.workflow.entities.steps.Step.StepType;
 import com.anode.workflow.entities.workflows.WorkflowContext;
@@ -135,5 +136,129 @@ class DefaultWorkflowComponentFactoryTest {
         assertThatThrownBy(() -> factory.getObject(context))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("Component name cannot be null or empty");
+    }
+
+    @Test
+    void shouldReturnRouteForSRouteContext() {
+        // Given
+        InvokableRoute mockRoute = mock(InvokableRoute.class);
+        WorkflowContext context = mock(WorkflowContext.class);
+        when(context.getCompType()).thenReturn(StepType.S_ROUTE);
+        when(context.getCompName()).thenReturn("testRoute");
+        when(mockContext.getBeansOfType(InvokableRoute.class))
+            .thenReturn(java.util.Map.of("testRoute", mockRoute));
+
+        // Re-initialize factory to pick up the mock beans
+        factory = new DefaultWorkflowComponentFactory(mockContext);
+        factory.init();
+
+        // When
+        Object result = factory.getObject(context);
+
+        // Then
+        assertThat(result).isEqualTo(mockRoute);
+    }
+
+    @Test
+    void shouldReturnRouteForPRouteContext() {
+        // Given
+        InvokableRoute mockRoute = mock(InvokableRoute.class);
+        WorkflowContext context = mock(WorkflowContext.class);
+        when(context.getCompType()).thenReturn(StepType.P_ROUTE);
+        when(context.getCompName()).thenReturn("testParallelRoute");
+        when(mockContext.getBeansOfType(InvokableRoute.class))
+            .thenReturn(java.util.Map.of("testParallelRoute", mockRoute));
+
+        // Re-initialize factory to pick up the mock beans
+        factory = new DefaultWorkflowComponentFactory(mockContext);
+        factory.init();
+
+        // When
+        Object result = factory.getObject(context);
+
+        // Then
+        assertThat(result).isEqualTo(mockRoute);
+    }
+
+    @Test
+    void shouldReturnRouteForPRouteDynamicContext() {
+        // Given
+        InvokableRoute mockRoute = mock(InvokableRoute.class);
+        WorkflowContext context = mock(WorkflowContext.class);
+        when(context.getCompType()).thenReturn(StepType.P_ROUTE_DYNAMIC);
+        when(context.getCompName()).thenReturn("testDynamicRoute");
+        when(mockContext.getBeansOfType(InvokableRoute.class))
+            .thenReturn(java.util.Map.of("testDynamicRoute", mockRoute));
+
+        // Re-initialize factory to pick up the mock beans
+        factory = new DefaultWorkflowComponentFactory(mockContext);
+        factory.init();
+
+        // When
+        Object result = factory.getObject(context);
+
+        // Then
+        assertThat(result).isEqualTo(mockRoute);
+    }
+
+    @Test
+    void shouldThrowExceptionForPauseType() {
+        // Given
+        WorkflowContext context = mock(WorkflowContext.class);
+        when(context.getCompType()).thenReturn(StepType.PAUSE);
+        when(context.getCompName()).thenReturn("testPause");
+
+        // When/Then
+        assertThatThrownBy(() -> factory.getObject(context))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("Unsupported component type: PAUSE")
+            .hasMessageContaining("should be handled by the Runtime Service");
+    }
+
+    @Test
+    void shouldThrowExceptionForPJoinType() {
+        // Given
+        WorkflowContext context = mock(WorkflowContext.class);
+        when(context.getCompType()).thenReturn(StepType.P_JOIN);
+        when(context.getCompName()).thenReturn("testJoin");
+
+        // When/Then
+        assertThatThrownBy(() -> factory.getObject(context))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("Unsupported component type: P_JOIN")
+            .hasMessageContaining("should be handled by the Runtime Service");
+    }
+
+    @Test
+    void shouldThrowExceptionForPersistType() {
+        // Given
+        WorkflowContext context = mock(WorkflowContext.class);
+        when(context.getCompType()).thenReturn(StepType.PERSIST);
+        when(context.getCompName()).thenReturn("testPersist");
+
+        // When/Then
+        assertThatThrownBy(() -> factory.getObject(context))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("Unsupported component type: PERSIST")
+            .hasMessageContaining("should be handled by the Runtime Service");
+    }
+
+    @Test
+    void shouldThrowExceptionForMissingRoute() {
+        // Given
+        WorkflowContext context = mock(WorkflowContext.class);
+        when(context.getCompType()).thenReturn(StepType.S_ROUTE);
+        when(context.getCompName()).thenReturn("nonExistentRoute");
+        when(mockContext.getBeansOfType(InvokableRoute.class))
+            .thenReturn(java.util.Map.of());
+
+        // Re-initialize factory
+        factory = new DefaultWorkflowComponentFactory(mockContext);
+        factory.init();
+
+        // When/Then
+        assertThatThrownBy(() -> factory.getObject(context))
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessageContaining("No InvokableRoute bean found");
     }
 }
