65e3cf49017ffef8a462c061ea4f69999947716d
A-N-O-D-E-R <weber.anoder@protonmail.com>
2025-11-15 19:45:59 +0100

    :bug; add AliasFor in custom Task annotation

--- src/main/java/com/anode/workflow/spring/autoconfigure/annotations/Task.java
+++ src/main/java/com/anode/workflow/spring/autoconfigure/annotations/Task.java
@@ -4,12 +4,14 @@ package com.anode.workflow.spring.autoconfigure.annotations;
 import java.lang.annotation.*;
 
 import org.springframework.stereotype.Component;
+import org.springframework.core.annotation.AliasFor;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Component
 public @interface Task {
+    @AliasFor(annotation= Component.class, attribute = "value")
     String value() default "";
     int order() default 0;
     String userData() default "";
